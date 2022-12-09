76c5860de4444df8b6fb11c368ded76054033903
jessica <57881206+fat-beaver@users.noreply.github.com>
2022-12-09 13:54:49 +1300

    Added subtraction.

--- src/Startup.java
+++ src/Startup.java
@@ -50,8 +50,21 @@ public class Startup {
         components.add(byteAdder);
         byteAdder.getSupply().addConnection(supply);
 
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        Switch operation = new Switch();
+        components.add(operation);
+        operation.getIn().addConnection(supply);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        pane.add(operation.getVisuals(), constraints);
+        constraints.gridy = 1;
+        pane.add(new JLabel("Subtraction enabled"), constraints);
+
+        constraints.weightx = 1;
+        constraints.weighty = 1;
         for (int i = 0; i < ByteAdder.WORD_SIZE; i++) {
-            constraints.gridy = 1;
+            constraints.gridy = 2;
             Switch topSwitch = new Switch();
             components.add(topSwitch);
             topSwitch.getIn().addConnection(supply);
@@ -59,27 +72,40 @@ public class Startup {
             constraints.gridx = ByteAdder.WORD_SIZE + 1 - i;
             pane.add(topSwitch.getVisuals(), constraints);
 
-            constraints.gridy = 2;
+            constraints.gridy = 3;
             Switch bottomSwitch = new Switch();
             components.add(bottomSwitch);
             bottomSwitch.getIn().addConnection(supply);
-            bottomSwitch.getOut().addConnection(byteAdder.getInTwo(i));
             constraints.gridx = ByteAdder.WORD_SIZE + 1 - i;
             pane.add(bottomSwitch.getVisuals(), constraints);
 
-            constraints.gridy = 3;
+            XORGate subtractionGate = new XORGate();
+            components.add(subtractionGate);
+            subtractionGate.getSupply().addConnection(supply);
+            subtractionGate.getInputOne().addConnection(operation.getOut());
+            subtractionGate.getInputTwo().addConnection(bottomSwitch.getOut());
+            subtractionGate.getOutput().addConnection(byteAdder.getInTwo(i));
+
+            constraints.gridy = 4;
             Light light = new Light();
             components.add(light);
             light.getInput().addConnection(byteAdder.getSumOut(i));
             constraints.gridx = ByteAdder.WORD_SIZE + 1 - i;
             pane.add(light.getVisuals(), constraints);
         }
+
         Light carryLight = new Light();
         components.add(carryLight);
-        byteAdder.getCarryOut().addConnection(carryLight.getInput());
+
+        XORGate carryXor = new XORGate();
+        components.add(carryXor);
+        carryXor.getSupply().addConnection(supply);
+        carryXor.getInputOne().addConnection(byteAdder.getCarryOut());
+        carryXor.getInputTwo().addConnection(operation.getOut());
+        carryXor.getOutput().addConnection(carryLight.getInput());
 
         constraints.gridx = 0;
-        constraints.gridy = 3;
+        constraints.gridy = 4;
         pane.add(carryLight.getVisuals(), constraints);
     }
 }
