daaa15f948a821705b72e6e3f4e661ff147b3c92
jessica <57881206+fat-beaver@users.noreply.github.com>
2022-12-09 13:54:50 +1300

    Last commit was missing changes to Startup

--- src/Startup.java
+++ src/Startup.java
@@ -63,26 +63,36 @@ public class Startup extends Component{
         subComponents.add(workingMemory);
         workingMemory.getSupply().addWithoutCheck(supply);
 
-        ControlPanel controlPanel = new ControlPanel(pane, constraints, TwoFiftySixByteCell.ADDRESS_SIZE, TwoFiftySixByteCell.WORD_SIZE);
+        DualSourceMemory programMemory = new DualSourceMemory();
+        subComponents.add(programMemory);
+        programMemory.getSupply().addWithoutCheck(supply);
+
+        ControlPanel controlPanel = new ControlPanel(pane, constraints, TwoFiftySixWordCell.ADDRESS_SIZE, TwoFiftySixWordCell.WORD_SIZE);
         subComponents.add(controlPanel);
         controlPanel.getSupply().addWithoutCheck(supply);
-        controlPanel.getWrite().addConnection(workingMemory.getWriteTwo());
+        controlPanel.getRAMWrite().addConnection(workingMemory.getWriteTwo());
+        controlPanel.getProgramWrite().addConnection(programMemory.getWriteTwo());
         controlPanel.getOverride().addConnection(workingMemory.getSelect());
+        controlPanel.getOverride().addConnection(programMemory.getSelect());
 
-        for (int i = 0; i < TwoFiftySixByteCell.ADDRESS_SIZE; i++) {
+        for (int i = 0; i < TwoFiftySixWordCell.ADDRESS_SIZE; i++) {
             workingMemory.getAddressTwo(i).addConnection(controlPanel.getAddress(i));
         }
-        for (int i = 0; i < TwoFiftySixByteCell.WORD_SIZE; i++) {
+        for (int i = 0; i < TwoFiftySixWordCell.WORD_SIZE; i++) {
             workingMemory.getWordTwo(i).addConnection(controlPanel.getWord(i));
+            workingMemory.getWordOut(i).addConnection(controlPanel.getWordDisplay(i));
+            programMemory.getWordTwo(i).addConnection(controlPanel.getWord(i));
+            programMemory.getWordOut(i).addConnection(controlPanel.getInstructionDisplay(i));
         }
 
         EightBitCounter addressCounter = new EightBitCounter();
         subComponents.add(addressCounter);
         addressCounter.getSupply().addWithoutCheck(supply);
         addressCounter.getClear().addConnection(controlPanel.getClear());
-        addressCounter.getClock().addConnection(clock.getOut());
-        for (int i = 0; i < TwoFiftySixByteCell.ADDRESS_SIZE; i++) {
+        for (int i = 0; i < TwoFiftySixWordCell.ADDRESS_SIZE; i++) {
             addressCounter.getOutput(i).addConnection(workingMemory.getAddressOne(i));
+            addressCounter.getOutput(i).addConnection(programMemory.getAddressOne(i));
+            controlPanel.getAddressDisplay(i).addConnection(workingMemory.getAddressOut(i));
         }
 
         SixteenBitAdder adder = new SixteenBitAdder();
@@ -96,12 +106,25 @@ public class Startup extends Component{
         latch.getClear().addConnection(controlPanel.getClear());
         latch.getClock().addConnection(clock.getOut());
 
-        for (int i = 0; i < TwoFiftySixByteCell.WORD_SIZE; i++) {
+        for (int i = 0; i < TwoFiftySixWordCell.WORD_SIZE; i++) {
             adder.getInOne(i).addConnection(workingMemory.getWordOut(i));
             adder.getInTwo(i).addConnection(latch.getOut(i));
             adder.getSumOut(i).addConnection(latch.getIn(i));
-            latch.getOut(i).addConnection(controlPanel.getDisplay(i));
+            latch.getOut(i).addConnection(controlPanel.getAccumulatorDisplay(i));
         }
+
+        TwoFiftySixBitAddresser instructionSelector = new TwoFiftySixBitAddresser();
+        subComponents.add(instructionSelector);
+        instructionSelector.getSupply().addWithoutCheck(supply);
+        for (int i = 0; i < TwoFiftySixBitAddresser.NUMBER_OF_BITS; i++) {
+            instructionSelector.getIn(i).addConnection(clock.getOut()); //only send clock signals to the current instruction
+        }
+        for (int i = 0; i < TwoFiftySixBitAddresser.ADDRESS_SIZE; i++) {
+            instructionSelector.getAddress(i).addConnection(programMemory.getWordOut(i));
+        }
+
+        instructionSelector.getOut(0).addConnection(addressCounter.getClock()); //NOP
+
         supply.checkState();
     }
 }
